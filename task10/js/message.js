d095d5023eecfcc85403b1ee3eb1e89ed6b7edb9
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2018-07-18 18:00:36 +0800

    修改bug

--- task10/js/message.js
+++ task10/js/message.js
@@ -1,5 +1,6 @@
 ! function () {
     var view = document.querySelector('section#messages')
+
     var model = {
         fetch: function () {
             var query = new AV.Query('Message')
@@ -8,7 +9,7 @@
         sava: function (name, content) {
             var Message = AV.Object.extend('Message');
             var message = new Message()
-            message.save({
+            return message.save({
                 name: name,
                 content: content
             })
@@ -22,46 +23,45 @@
             })
         },
     }
+
+
     var controller = {
         view: null,
         model: null,
+        messageList: null,
         init: function (view, model) {
             this.view = view
             this.model = model
             this.model.init()
             this.messageList = view.querySelector('#messageList')
             this.form = view.querySelector('form')
-
             this.loadMassage()
             this.bindEvents()
         },
-
         loadMassage: function () {
             this.model.fetch().then((messages) => {
                 let array = messages.map((item) => item.attributes)
                 array.forEach((item) => {
                     let li = document.createElement('li')
                     li.innerText = `${item.name}:${item.content}`
-                    let messageList = document.querySelector('#messageList')
-                    messageList.appendChild(li)
-                });
-            }, function (error) {
-                alert('提交失败')
+                    this.messageList.appendChild(li)
+                })
             })
         },
         bindEvents: function () {
-
-            this.form.addEventListener('submit', function (e) {
+            this.form.addEventListener('submit', (e)=> {
                 e.preventDefault()
                 this.saveMessage()
-
             })
         },
         saveMessage: function () {
-            let myForm = this.from
             let content = myForm.querySelector('input[name=content]').value
             let name = myForm.querySelector('input[name=name]').value
-            this.model.sava(name, content).then(function (object) {
+            console.log(name)
+            this.model.sava({
+                'name':name,
+                'content':content
+            }).then(function (object) {
                 let li = document.createElement('li')
                 li.innerText = `${object.attributes.name}:${object.attributes.content}`
                 let messageList = document.querySelector('#messageList')
@@ -70,11 +70,8 @@
                 myForm.querySelector('input[name=name]').value = ''
             })
         }
-
     }
-
     controller.init(view, model)
-
 }.call()
 
 
